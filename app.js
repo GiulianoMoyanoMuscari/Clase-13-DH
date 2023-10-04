351c2c9876918685a34eb72ff48d4dae0ae9ec16
Fatima Diaz <fatima-diaz@live.com.ar>
2023-10-04 16:51:22 -0300

    corregido app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-let dia = "Miercoles"
+let dia1 = "Miercoles";
 
-let actividad = "Tenemos clase de de Digital House";
+let actividad = "Tenemos clase  de Digital House";
 
-if (dia === "Miercoles"){
+if (dia1 === "Miercoles"){
     console.log(actividad)
 }
+
 let dia = "hoy es Miercoles"
 function semana() {
     dia1: "lunes";
     dia2: "martes";
     dia3: "miercoles";
 }
-.console.log(dia);
+console.log(dia);
